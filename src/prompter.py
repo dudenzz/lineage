e8252610b6718bb450b802bfdb2f4d50c028e7db
dudenzz <kubadt@gmail.com>
2025-03-18 14:40:03 +0100

    minimalistic graph

--- src/prompter.py
+++ src/prompter.py
@@ -2,6 +2,8 @@ import os
 from openai import OpenAI
 from prompting_lib import Prompt
 import tqdm
+import json
+from os import path
 with open('secret', 'r') as f:
     key = f.read()
 
@@ -12,8 +14,18 @@ client = OpenAI(api_key=key)
 # initial.prompt(client)
 # initial.save()
 
-for section_no in tqdm.tqdm(range(1,11)):
-    prompt = Prompt(f'prompts/create{section_no}.json')
-    prompt.prompt(client)
-    prompt.save()
-    
+# for section_no in tqdm.tqdm(range(1,11)):
+#     prompt = Prompt(f'prompts/create{section_no}.json')
+#     prompt.prompt(client)
+#     prompt.save()
+with open('prompts/transform.json','r') as json_file:
+            data = json.load(json_file)
+            context = data['context']
+            task = data['task']
+            code = open('src/SQL_scripts/transformation1.1.sql').read()
+            sample_input = open('examples/example1.transformation').read()
+            sample_output = open('examples/example1.lineage_adaptation').read()
+            name = path.basename('src/SQL_scripts/transformation1.1.sql')
+            transform_prompt = Prompt(context, task, code, sample_input, sample_output, name)
+            transform_prompt.prompt_with_code(client)
+            transform_prompt.save_transformed()
--- src/prompting_lib.py
+++ src/prompting_lib.py
@@ -6,12 +6,52 @@ class Prompt:
         self.context = context
         self.task = task
         self.name = name
-    def __init__(self, filename):
-        with open(filename,'r') as json_file:
-            data = json.load(json_file)
-            self.context = data['context']
-            self.task = data['task']
-            self.name = path.basename(filename)
+    def __init__(self, context, task, sinp, sout, code, name):
+        self.context = context
+        self.code = code
+        self.task = task
+        self.name = name
+        self.sinp = sinp
+        self.sout = sout
+        
+    # def __init__(self, filename):
+    #     with open(filename,'r') as json_file:
+    #         data = json.load(json_file)
+    #         self.context = data['context']
+    #         self.task = data['task']
+    #         self.name = path.basename(filename)
+    def prompt_with_code(self, openai_client : openai.Client):
+        chat_completion = openai_client.chat.completions.create(
+        messages=[
+            {
+                "role" : 'system',
+                "content": self.context
+            },
+            {
+                "role" : 'system',
+                "content": self.task
+            },
+            
+                        {
+                "role" : 'system',
+                "content": self.sinp
+            },
+            
+                        {
+                "role" : 'system',
+                "content": self.sout
+            }
+            ,
+                        {
+                "role" : 'system',
+                "content": self.code
+            }
+        ],
+        model="gpt-4o",
+        n=5
+        )
+        self.answers = {}
+        self.answers[0] = chat_completion.choices[0].message.content
     def prompt(self, openai_client : openai.Client):
         chat_completion = openai_client.chat.completions.create(
         messages=[
@@ -25,9 +65,16 @@ class Prompt:
             }
         ],
         model="gpt-4o",
+        n=1
         )
-        self.answer = chat_completion.choices[0].message.content
+        self.answers = {}
+        self.answers[0] = chat_completion.choices[0].message.content
+    def save_transformed(self):
+        for i in range(1,2):
+            with open(f'transformations/{self.name}.{i}.answer', 'w+', encoding='utf-8') as file:
+                file.write(self.answers[i-1])
     def save(self):
-        with open(f'answers/{self.name}.answer', 'w+') as file:
-            file.write(self.answer)
+        for i in range(1,6):
+            with open(f'answers/{self.name}.{i}.answer', 'w+', encoding='utf-8') as file:
+                file.write(self.answers[i-1])
 
